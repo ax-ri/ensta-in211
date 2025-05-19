247c39014b071b67043f59303236e2e0a89fd313
Axel Richard <axel.richard@ensta-paris.fr>
2025-05-19 21:18:07 +0200

    back: add persistent session

--- backend/services/session.js
+++ backend/services/session.js
@@ -1,4 +1,10 @@
+import sqlite3 from 'sqlite3';
 import session from 'express-session';
+import sqliteStoreFactory from 'express-session-sqlite';
+
+const SqliteStore = sqliteStoreFactory.default(session);
+
+console.log(process.cwd());
 export function main(app) {
   app.use(
     session({
@@ -6,13 +12,19 @@ export function main(app) {
       saveUninitialized: true,
       secret: process.env.SESSION_SECRET || 'session-secret',
       cookie: {
-        secure: false, //process.env.NODE_ENV !== 'development',
+        secure: false,
         httpOnly: true,
       },
-      // store: new TypeormStore({
-      //   cleanupLimit: 2,
-      //   ttl: 86400,
-      // }).connect(sessionRepository),
+      store: new SqliteStore({
+        driver: sqlite3.Database,
+        // for in-memory database
+        // path: ':memory:'
+        path: '/tmp/sessions.sqlite',
+        // Session TTL in milliseconds
+        ttl: 5_000,
+        prefix: 'sess:',
+        cleanupInterval: 30_000,
+      }),
     }),
   );
 }
